7ecbc20be46428751f2b26c657c0365a12e51e4c
covenantcodes <covenantcodes@gmail.com>
2025-07-16 18:21:03 +0100

    Add font constants for typography management
    - Introduced FONTFAMILY object with various font weights and styles.
    - Added FONTSIZE object to define standard font sizes.
    - Created LINEHEIGHT object to standardize line heights for text.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
-export default function App() {
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    JostThin: require("./assets/fonts/Jost-Thin.ttf"),
+    JostThinItalic: require("./assets/fonts/Jost-ThinItalic.ttf"),
+    JostLight: require("./assets/fonts/Jost-Light.ttf"),
+    JostExtraLight: require("./assets/fonts/Jost-ExtraLight.ttf"),
+    JostMedium: require("./assets/fonts/Jost-Medium.ttf"),
+    JostMediumItalic: require("./assets/fonts/Jost-MediumItalic.ttf"),
+    JostRegular: require("./assets/fonts/Jost-Regular.ttf"),
+    JostSemiBold: require("./assets/fonts/Jost-SemiBold.ttf"),
+    JostBold: require("./assets/fonts/Jost-Bold.ttf"),
+    JostExtraBold: require("./assets/fonts/Jost-ExtraBold.ttf"),
+    JostExtraBoldItalic: require("./assets/fonts/Jost-ExtraBoldItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
---
+++ utils/fonts.ts
@@ -0,0 +1,28 @@
+
+export const FONTFAMILY = {
+    bold: "JostBold",
+    regular: "JostRegular",
+    thin: "JostThin",
+    light: "JostLight",
+    semibold: "JostSemiBold",
+    heavy: "JostExtraBold",
+    medium: "JostMedium",
+    BricolageSemibold: "BricolageGrotesqueSemiBold"
+  };
+
+export const FONTSIZE = {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+    xxl: 20,
+    xxxl: 22,
+};
+
+  export const LINEHEIGHT = {
+    tight: 16,
+    normal: 20,
+    relaxed: 24,
+  };
+  
